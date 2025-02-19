18ce1343a1f3a4f98ea8f793dadb77b80a770c85
Nick Klingensmith <programmerpichu@gmail.com>
2025-02-18 17:05:49 -0800

    Update demo with some tools for playing with perf

--- Program.cs
+++ Program.cs
@@ -8,63 +8,114 @@ using BepuUtilities.Memory;
 
 using Matrix = StereoKit.Matrix;
 using Mesh   = StereoKit.Mesh;
-using Sphere = BepuPhysics.Collidables.Sphere;
+using Box    = BepuPhysics.Collidables.Box;
 
-// Initialize StereoKit
-if (!SK.Initialize(new SKSettings {
-		appName      = "StereoKit Physics",
-		assetsFolder = "Assets",
-		origin	     = OriginMode.Floor,
-	})) return;
-
-// Setup physics
-BufferPool pool    = new();
-Simulation sim     = Simulation.Create(pool, new NarrowPhaseCallbacks(), new PoseIntegratorCallbacks(new Vec3(0, -10, 0)), new SolveDescription(8, 1));
-double     simTime = 0;
-float      simStep = 1.0f/90.0f;
-ThreadDispatcher dispatcher = new ThreadDispatcher(Environment.ProcessorCount);
-
-// Make a solid floor
-Material floorMaterial = new Material("floor.hlsl");
-floorMaterial.Transparency = Transparency.Blend;
-Vec3 floorSize = new(200,20,200);
-Vec3 floorPos  = new(0,-10,0);
-sim.Statics.Add(new StaticDescription(floorPos, Quat.Identity, sim.Shapes.Add(new Box(floorSize.x, floorSize.y, floorSize.z))));
-
-// Add some spheres!
-float sphereSize = 0.1f;
-List<BodyHandle> bodies = new();
-var sphere        = new Sphere(sphereSize*0.5f);
-var sphereInertia = sphere.ComputeInertia(1);
-var sphereIdx     = sim.Shapes.Add(sphere);
-for	(int x=-1; x<=1; x+=1) { for (int y=-1; y<=1; y+=1) { for (int z=0; z<=4; z+=1) {
-	RigidPose pose = new RigidPose();
-	pose.Orientation = Quat.Identity;
-	pose.Position    = new Vec3(x , z*2, y) * sphereSize + new Vec3(0,0,-2);
-	var sphereDesc = BodyDescription.CreateDynamic(
-		pose,
-		sphereInertia,
-		new CollidableDescription(sphereIdx, 0.1f), new BodyActivityDescription(0.01f));
-	bodies.Add(sim.Bodies.Add(sphereDesc));
-}}}
-
-// Core application loop
-SK.Run(() =>
+public static class Program
 {
-	// Step the physics simulation
-	while (simTime < Time.Total)
+	public static void Main(string[] args)
 	{
-		sim.Timestep(simStep, dispatcher);
-		simTime += simStep;
-	}
+		// Initialize StereoKit
+		if (!SK.Initialize(new SKSettings {
+				appName      = "StereoKit Physics",
+				assetsFolder = "Assets",
+				origin       = OriginMode.Floor,
+			})) return;
 
-	// Draw the floor
-	if (SK.System.displayType == Display.Opaque)
-		Mesh.Cube.Draw(floorMaterial, Matrix.TS(floorPos, floorSize));
-	
-	// Get the positions from physics, and draw the spheres
-	foreach(var body in bodies) {
-		var desc = sim.Bodies.GetDescription(body);
-		Mesh.Sphere.Draw(Material.Default, Matrix.TRS(desc.Pose.Position, desc.Pose.Orientation, sphereSize));
+		Pose windowPose = new Pose(0,1.5f,-0.5f, Quat.LookDir(0,0,1));
+
+		// Setup physics
+		BufferPool       pool       = new();
+		Simulation       sim        = Simulation.Create(pool, new NarrowPhaseCallbacks(), new PoseIntegratorCallbacks(new Vec3(0, -10, 0)), new SolveDescription(2, 1));
+		ThreadDispatcher dispatcher = new(Environment.ProcessorCount);
+
+		// Make a solid floor
+		Material floorMaterial = new Material("floor.hlsl");
+		floorMaterial.Transparency = Transparency.Blend;
+		Vec3 floorSize = new(200,20,200);
+		Vec3 floorPos  = new(0,-10,0);
+		sim.Statics.Add(new StaticDescription(floorPos, Quat.Identity, sim.Shapes.Add(new Box(floorSize.x, floorSize.y, floorSize.z))));
+
+		// Variables for the UI and experience
+		float boxSize    = 0.1f;
+		float layerCount = 2;
+		float velocityIt = sim.Solver.VelocityIterationCount;
+		float substeps   = sim.Solver.SubstepCount;
+		float simFPS     = 60;
+		List<BodyHandle> bodies = new();
+
+		double     simTime = 0;
+		float      simStep = 1.0f/simFPS;
+
+		// Core application loop
+		SK.Run(() =>
+		{
+			// Step the physics simulation
+			while (simTime < Time.Total)
+			{
+				sim.Timestep(simStep, dispatcher);
+				simTime += simStep;
+			}
+
+			// Draw the floor
+			if (Device.DisplayBlend == DisplayBlend.Opaque)
+				Mesh.Cube.Draw(floorMaterial, Matrix.TS(floorPos, floorSize));
+			
+			// Get the positions from physics, and draw the spheres
+			foreach(var body in bodies) {
+				var desc = sim.Bodies.GetDescription(body);
+				Mesh.Cube.Draw(Material.Default, Matrix.TRS(desc.Pose.Position, desc.Pose.Orientation, boxSize));
+			}
+
+			// UI for working with the simulation
+			UI.WindowBegin("Simulation", ref windowPose);
+
+			// Spawn a grid of boxes
+			if (UI.Button("Spawn Objects")) {
+				Box         box        = new (boxSize, boxSize, boxSize);
+				BodyInertia boxInertia = box.ComputeInertia(1);
+				TypedIndex  boxIdx     = sim.Shapes.Add(box);
+				for	(int x=-1; x<=1; x+=1) { for (int y=-1; y<=1; y+=1) { for (int z=0; z<=layerCount; z+=1) {
+					bodies.Add(sim.Bodies.Add(BodyDescription.CreateDynamic(
+						new RigidPose(new Vec3(x, z*2, y) * boxSize + new Vec3(0,4,-2), Quat.Identity),
+						boxInertia,
+						new CollidableDescription(boxIdx, 0.1f), new BodyActivityDescription(0.01f))));
+				}}}
+			}
+			// Let the user configure ths size of the grid
+			UI.Label($"Layers: {(int)layerCount}");
+			UI.HSlider("Layers", ref layerCount, 0, 10, 1);
+
+			// And let them clear things out
+			if (UI.Button("Clear")) {
+				for (int i=0; i<bodies.Count; i+=1) {
+					sim.Bodies.Remove(bodies[i]);
+				}
+				bodies.Clear();
+			}
+
+			// Display some info about the current simulation
+			UI.HSeparator();
+			
+			UI.Label($"Active: {sim.Bodies.ActiveSet.Count}");
+
+			// This method of counting sleeping objects seems to include bodies
+			// that have been deleted in some kind of pooling strategy, so it's
+			// not accurate after a 'Clear'
+			int sleeping = 0;
+			for (int i=1; i<sim.Bodies.Sets.Length; i+=1)
+				sleeping += sim.Bodies.Sets[i].Count;
+			UI.Label($"Sleeping: {sleeping}");
+
+			// Some options for tweaking perf/quality at runtime for testing
+			UI.Label($"Velocity iterations: {sim.Solver.VelocityIterationCount}");
+			if (UI.HSlider("velocity", ref velocityIt, 1, 8, 1)) sim.Solver.VelocityIterationCount = (int)velocityIt;
+			UI.Label($"Substeps: {sim.Solver.SubstepCount}");
+			if (UI.HSlider("substeps", ref substeps, 1, 8, 1)) sim.Solver.SubstepCount = (int)substeps;
+			UI.Label($"Physics FPS: {simFPS}");
+			if (UI.HSlider("fps", ref simFPS, 24, 90, 1)) simStep = 1.0f/simFPS;
+			
+
+			UI.WindowEnd();
+		});
 	}
-});
+}
